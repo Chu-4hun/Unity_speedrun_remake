a669ac70636179649f403871d84557952513c427
Chu-4hun <tapok134224@mail.ru>
2022-09-22 20:06:39 +0300

    Fix menu

--- Assets/scripts/GameManager.cs
+++ Assets/scripts/GameManager.cs
@@ -9,30 +9,19 @@ public class GameManager : MonoBehaviour
     public Text Score;
     public Text PlayerNickname;
     public PlayerMovement player;
-    public bool isUIScene;
 
     private void OnApplicationFocus(bool hasFocus)
     {
-        if (hasFocus && !isUIScene)
-        {
-            Cursor.lockState = CursorLockMode.Locked;
-        }
-        else
-        {
-            Cursor.lockState = CursorLockMode.None;
-        }
-        
+        Cursor.lockState = hasFocus ? CursorLockMode.Locked : CursorLockMode.None;
     }
 
     private void Start()
     {
-        if (!isUIScene)
-            Cursor.lockState = CursorLockMode.Locked;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     private void Update()
     {
-        if (isUIScene)return;
         slider.value = player.HP;
         Score.text = "Score: " + player.coins;
         PlayerNickname.text = "Name: " + GlobalVariables.Get<string>("name");
